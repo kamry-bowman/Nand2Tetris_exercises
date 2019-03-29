fedd67d3fa9900cafcb05a24c715e0d841f0bc9f
Kamry Bowman <kamry.bowman@gmail.com>
2019-03-29 12:17:33 -0600

    Machine code to turn screen on and off

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,57 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+  @on
+  M=0;
+  M=D
+(LOOP)
+  @KBD
+  D=M
+  @HANDLEOFF
+  D;JEQ   // handleoff subroutine if KBD == 0
+(HANDLEON)
+  @on
+  D=M
+  @LOOP
+  D;JGT // if screen is already on, goto LOOP
+  @8191
+  D=A
+  @i
+  M=D
+(ONPAINTLOOP)
+  @on
+  M=1;
+  @i
+  D=M
+  @LOOP
+  D;JEQ
+  @SCREEN
+  A=D+A   //i+KBD_index, saving address
+  M=-1     //set memory at i+KBD_index off
+  @i
+  M=M-1;  // i = i-1
+  @ONPAINTLOOP
+  0;JMP
+(HANDLEOFF)
+  @on
+  D=M
+  @LOOP
+  D;JEQ // if screen is already off, goto LOOP
+  @8191
+  D=A
+  @i
+  M=D
+(OFFPAINTLOOP)
+  @on
+  M=0;
+  @i
+  D=M
+  @LOOP
+  D;JEQ
+  @SCREEN
+  A=D+A   //i+KBD_index, saving address
+  M=0     //set memory at i+KBD_index off
+  @i
+  M=M-1;  // i = i-1
+  @OFFPAINTLOOP
+  0;JMP
