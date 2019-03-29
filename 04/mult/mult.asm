c3d14e8c32ace2b6ae87c6e4af6edc5baa4548a4
Kamry Bowman <kamry.bowman@gmail.com>
2019-03-29 00:01:26 -0600

    Wrote first asm program

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,33 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+  @R2
+  M=0
+  @i
+  M=1
+  @total
+  M=0
+(LOOP)
+  @i
+  D=M
+  @R1
+  D=M-D
+  @POSTLOOP
+  D;JLT // Exit Loop if R1-i == 0
+  @R0
+  D=M     // D=@R0
+  @total
+  M=D+M   // total = total + @R0
+  @i
+  M=M+1
+  @LOOP
+  0;JMP
+(POSTLOOP)
+  @total
+  D=M
+  @R2
+  M=D
+(END)
+  @END
+  0;JMP
+
